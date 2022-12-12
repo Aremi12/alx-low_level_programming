09fc2fc5ce96b52cb9359a30235adf509575503e
Aremi12 <ibrahimmuhammad1ib@gmail.com>
2022-12-12 12:32:02 -0800

    mycommit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Entry
- * Return - Always 0
+ * Return: - Always 0
  */
 int main(void)
 {
@@ -26,6 +26,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %dis %d and is less than 6 and not 0\n", n, a);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 		return (0);
 	}
+}
