232758cb202872efff07552a9cb245be07b08f3c
Aremi12 <ibrahimmuhammad1ib@gmail.com>
2022-12-15 00:40:28 -0800

    mycommit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@ void times_table(void)
 {
 	int rone, cone, d;
 
-	for (rone = 0; rone <= 9; cone++)
+	for (rone = 0; rone <= 9; rone++)
 	{
 		_putchar('0');
 		_putchar(',');
@@ -36,4 +36,5 @@ void times_table(void)
 		}
 		_putchar('\n');
 	}
+
 }
