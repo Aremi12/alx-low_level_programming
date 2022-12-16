8d50f507895b38765d156abf9433828660ef5082
Aremi12 <ibrahimmuhammad1ib@gmail.com>
2022-12-16 06:40:13 -0800

    mycommit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98 - Entry point
+ * print_to_98 - a function that prints numbers from n to 98
  * user input's number prints to 98, regardless < 98 or > 98
  * @n: number input
  * Return: Always 0
