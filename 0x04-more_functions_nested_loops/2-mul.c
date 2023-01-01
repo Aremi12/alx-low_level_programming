1af81e122a9f826fcd3c43e4c232fe014fe10e1d
Aremi12 <ibrahimmuhammad1ib@gmail.com>
2023-01-01 03:28:00 -0800

    function that multiplies two integres

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * mul - function that multiplies two int
+ * mul - a function that multiplies two integers
  * @a: first input
  * @b: second input
  * Return: multiplication result of the 2 inputs
@@ -9,5 +9,5 @@
 
 int mul(int a, int b)
 {
-	return (a * a);
+	return (a * b);
 }
